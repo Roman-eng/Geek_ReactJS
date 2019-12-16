cd265a367e44a46947043b9d130442956a86538e
Roman-eng <roman-x-sport@ya.ru>
2019-12-17 00:42:18 +0800

    lesson3 с доработками_2

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Layout from './components/Layout'
+import Layout from './components/Layout.jsx'
 
 ReactDOM.render(
     <MuiThemeProvider>
